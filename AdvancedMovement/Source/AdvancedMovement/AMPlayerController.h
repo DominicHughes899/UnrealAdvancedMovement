234c58de56b14e73086d0064425768c0407baa28
DominicHughes899 <91923512+DominicHughes899@users.noreply.github.com>
2023-12-27 11:51:20 +0000

    Added PlayerController and input interface

---
+++ AdvancedMovement/Source/AdvancedMovement/AMPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AMPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVANCEDMOVEMENT_API AAMPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
